64270076a8f3a7d2422bc79e498bd3c599867f45
cssuryaa <surya_c@persistent.com>
2022-08-02 14:20:16 +0530

    first commit

---
+++ file.java
@@ -0,0 +1,11 @@
+package com.psl;
+
+public class Employee{
+	public void display(){
+	System.out.println("Addding display");
+	}
+	public void Apicall(){
+	System.out.println("Addding display");
+	}
+	
+}
